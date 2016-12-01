9e3a7c0b042e3b38c4aabbd70e787be01b1037f1
Rayka95 <rayka.1995@gmail.com>
2016-12-01 14:55:54 +0330

    Add files via upload

---
+++ core_function.php
@@ -0,0 +1,226 @@
+<?php
+            // C0ded by RAYKA95
+
+    function check($requested) {
+     
+                switch ($requested) {
+                case "ا":
+                    return 1;
+                    break;
+                case "آ":
+                    return 1;
+                    break;
+                case "ئ":
+                    return 1;
+                    break;
+                case "א":
+                    return 1;
+                    break;
+                case "ب":
+                    return 2;
+                    break;
+                case "ב":
+                    return 2;
+                    break;
+                case "پ":
+                    return 2;
+                    break;
+                case "ج":
+                    return 3;
+                    break;  
+                case "ג":
+                    return 3;
+                    break;  
+                case "چ":
+                    return 3;
+                    break;  
+                case "ﭻ":
+                    return 3;
+                    break;  
+                case "د":
+                    return 4;
+                    break;  
+                case "ד":
+                    return 4;
+                    break;  
+                case "ه":
+                    return 5;
+                    break;
+                case "ة":
+                    return 400;
+                    break;
+                case "ھ":
+                    return 5;
+                    break;  
+                case "ה":
+                    return 5;
+                    break;  
+                case "و":
+                    return 6;
+                    break;  
+                case "ו":
+                    return 6;
+                    break;  
+                case "ز":
+                    return 7;
+                    break;
+                case "ﺰ":
+                    return 7;
+                    break;
+                case "ז":
+                    return 7;
+                    break;
+                case "ژ":
+                    return 7;
+                    break;  
+                case "ح":
+                    return 8;
+                    break;  
+                case "ח":
+                    return 8;
+                    break;  
+                case "ط":
+                    return 9;
+                    break;  
+                case "ט":
+                    return 9;
+                    break;  
+                case "ی":
+                    return 10;
+                    break;
+                case "ﯿ":
+                    return 10;
+                    break;
+                case "י":
+                    return 10;
+                    break;
+                case "ي":
+                    return 10;
+                    break;
+                case "ک":
+                    return 20;
+                    break;  
+                case "כ":
+                    return 20;
+                    break;  
+                case "ك":
+                    return 20;
+                    break; 
+                case "گ":
+                    return 20;
+                    break;  
+                case "ﮔ":
+                    return 20;
+                    break;  
+                case "ل":
+                    return 30;
+                    break;  
+                case "ל":
+                    return 30;
+                    break;  
+                case "م":
+                    return 40;
+                    break;  
+                case "ﻢ":
+                    return 40;
+                    break;  
+                case "מ":
+                    return 40;
+                    break;  
+                case "ם":
+                    return 40;
+                    break;  
+                case "ن":
+                    return 50;
+                    break;  
+                case "ﻨ":
+                    return 50;
+                    break;  
+                case "נ":
+                    return 50;
+                    break;  
+                case "ן":
+                    return 50;
+                    break; 
+                case "س":
+                    return 60;
+                    break;  
+                case "ס":
+                    return 60;
+                    break;  
+                case "ع":
+                    return 70;
+                    break;  
+                case "ע":
+                    return 70;
+                    break;  
+                case "ف":
+                    return 80;
+                    break;          
+                case "פ":
+                    return 80;
+                    break;    
+                case "ף":
+                    return 80;
+                    break; 
+                case "ص":
+                    return 90;
+                    break;  
+                case "צ":
+                    return 90;
+                    break;  
+                case "ץ":
+                    return 90;
+                    break;  
+                case "ق":
+                    return 100;
+                    break;  
+                case "ק":
+                    return 100;
+                    break;  
+                case "ر":
+                    return 200;
+                    break;  
+                case "ﺮ":
+                    return 200;
+                    break;  
+                case "ר":
+                    return 200;
+                    break;  
+                case "ش":
+                    return 300;
+                    break;
+                case "ש":
+                    return 300;
+                    break;
+                case "ت":
+                    return 400;
+                    break;  
+                case "ת":
+                    return 400;
+                    break;  
+                case "ث":
+                    return 500;
+                    break;          
+                case "خ":
+                    return 600;
+                    break;  
+                case "ذ":
+                    return 700;
+                    break;  
+                case "ض":
+                    return 800;
+                    break;  
+                case "ظ":
+                    return 900;
+                    break;
+                case "غ":
+                    return 1000;
+                    break; 
+                default:
+                    return 0;
+            }
+        
+    }
+
+?>
